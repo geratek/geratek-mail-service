2f44d523fc3250a160ba3706ac3a77f3f72c0f11
lacau <lacau@zpay.com.br>
2020-09-17 15:49:01 -0300

    Fix CORS

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const ses = new aws.SES();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.post('/send', [
     body('name').notEmpty(),
